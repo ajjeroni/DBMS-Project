105fba3a4517b995efa62a3ed676e96f9add7d81
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-21 17:26:29 -0800

    Revise PROFESSOR table structure and data types
    Updated the PROFESSOR table schema to improve data types and constraints.

--- DDL/PROFESSOR.sql
+++ DDL/PROFESSOR.sql
@@ -1,14 +1,19 @@
-CREATE TABLE PROFESSOR(
-	SSN numeric(9) primary key,
-    FNAME varchar(20),
-    LNAME varchar(20),
-    SEX enum('M','F'),
-    TITLE varchar(20),
-    T_AREACODE numeric(3),
-    T_NUMBER numeric(7),
-    A_STREET varchar(20),
-    A_CITY varchar(20),
-    A_STATE varchar(13),
-    A_ZIP char(5),
-    SALARY numeric(6)
+CREATE TABLE PROFESSOR (
+    SSN CHAR(9) PRIMARY KEY,              -- we used CHAR instead of numeric becuase if SSN starts with zero, it is truncated
+    FNAME VARCHAR(20) NOT NULL,
+    LNAME VARCHAR(20) NOT NULL,
+    SEX ENUM('M', 'F') NOT NULL,
+    TITLE VARCHAR(30),
+    
+    -- Telephone componenets
+    T_AREACODE CHAR(3) NOT NULL,
+    T_NUMBER   CHAR(7) NOT NULL,
+
+    -- Address components
+    A_STREET VARCHAR(40) NOT NULL,
+    A_CITY   VARCHAR(30) NOT NULL,
+    A_STATE  CHAR(2) NOT NULL,            -- we use state abbreviations
+    A_ZIP    CHAR(5) NOT NULL,
+
+    SALARY DECIMAL(10,2) NOT NULL         -- numeric salary, supports cents
 );
