8e19e68bfa272419577e63a729a06e37e69c6835
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-24 13:10:01 -0800

    Add SQL insertion for new professor record

---
+++ DML/PROFESSOR_INSERTION.sql
@@ -0,0 +1,8 @@
+INSERT INTO 
+    PROFESSOR 
+        (`SSN`, `FNAME`, `LNAME`, `SEX`, `TITLE`, `T_AREACODE`, `T_NUMBER`, 
+        `A_STREET`, `A_CITY`, `A_STATE`, `A_ZIP`, `SALARY`)
+
+    VALUES 
+        ('123456789', 'John', 'Smith', 'M', 'Professor', '123', '4567890', 
+        'Street', 'Fullerton', 'CA', '90000', 100000.00)
