baa196fcfc679e7d8b53f87d5c39db9fa20a754d
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-24 15:47:50 -0800

    Fix duplicate record insertion in SQL script

---
+++ DML/RECORD_INSERTION.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+    RECORD
+        (`CWID`, `S_FNAME`, `S_LNAME`, `S_T_AREA_CODE`, `TS_T_NUMBER`,
+        `S_A_STREET`, `S_A_CITY`, `S_A_STATE`, `S_A_ZIP`, `MAJOR`)
+    VALUES 
+        ('000000001', 'Adam', 'Levine', 949, 999999,
+        'Home Avenue', 'Los Angeles', 'CA', '90000', 001)
