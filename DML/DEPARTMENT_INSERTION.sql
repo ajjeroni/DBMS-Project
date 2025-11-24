1615a7425aee312a21c2dbde490fd3aca65bf33f
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-24 13:26:59 -0800

    Add department insertion SQL script

---
+++ DML/DEPARTMENT_INSERTION.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+    DEPARTMENT
+        (`D_NUMBER`, `D_NAME`, `D_LOCATION`, `D_TELEPHONE`, `CHAIR_SSN`)
+    VALUES
+        ('001', 'Humanities', 'Unknown', '123-456-7890', '123456789')
