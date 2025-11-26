9ba8a15d81c01d3d069484687073a22cbedfc81a
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-26 12:17:20 -0800

    Insert multiple records into RECORD table

---
+++ table-data/RECORD_DATA.sql
@@ -0,0 +1,10 @@
+INSERT INTO RECORD VALUES
+('100000001','John','Kim',714,5551201,'12 Oak St','Fullerton','CA','92831',1),
+('100000002','Emily','Tran',657,5552202,'45 Pine Rd','Irvine','CA','92617',2),
+('100000003','Michael','Lopez',562,5553303,'77 Maple Ave','Long Beach','CA','90840',1),
+('100000004','Sarah','Park',714,5554404,'90 Walnut Ln','Brea','CA','92821',2),
+('100000005','David','Chen',657,5555505,'33 Elm St','Irvine','CA','92614',1),
+
+('100000006','Jessica','Nguyen',562,5556606,'10 Palm Dr','Long Beach','CA','90815',2),
+('100000007','Brian','Orozco',714,5557707,'81 Sycamore St','Fullerton','CA','92832',1),
+('100000008','Alyssa','Diaz',657,5558808,'18 Birch Ct','Tustin','CA','92782',2);
