008c3aeb03fb1e9525aa3795008bd071ba3c1da9
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-26 12:14:49 -0800

    Add initial professor data to database

---
+++ table-data/PROFESSOR_DATA.sql
@@ -0,0 +1,50 @@
+INSERT INTO PROFESSOR
+VALUES
+(
+    '012345678',        -- SSN
+    'Alan',             -- FNAME
+    'Turing',           -- LNAME
+    'M',                -- SEX
+    'Associate Professor',
+    '714',              -- T_AREACODE
+    '5551234',          -- T_NUMBER
+    '123 Computing Ln', -- A_STREET
+    'Fullerton',        -- A_CITY
+    'CA',               -- A_STATE
+    '92831',            -- A_ZIP
+    95000.00            -- SALARY
+);
+
+INSERT INTO PROFESSOR
+VALUES
+(
+    '987654321',
+    'Grace',
+    'Hopper',
+    'F',
+    'Professor',
+    '657',
+    '5559876',
+    '456 Algorithm Ave',
+    'Irvine',
+    'CA',
+    '92617',
+    130000.00
+);
+
+INSERT INTO PROFESSOR
+VALUES
+(
+    '001223344',
+    'Ada',
+    'Lovelace',
+    'F',
+    'Assistant Professor',
+    '562',
+    '5551122',
+    '789 Logic Blvd',
+    'Long Beach',
+    'CA',
+    '90840',
+    87000.00
+);
