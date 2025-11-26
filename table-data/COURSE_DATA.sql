54c939956d8dd18bce12b8aaf560635898b4341f
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-26 12:16:03 -0800

    Add initial COURSE_DATA.sql with course entries

---
+++ table-data/COURSE_DATA.sql
@@ -0,0 +1,39 @@
+INSERT INTO COURSE
+VALUES
+(
+    100,                       -- C_NUMBER
+    'Introduction to Programming',
+    3,                         -- UNIT
+    'Python Crash Course',
+    1                          -- OFFERED_BY (Computer Science)
+);
+
+INSERT INTO COURSE
+VALUES
+(
+    210,
+    'Data Structures and Algorithms',
+    4,
+    'Algorithms (Sedgewick & Wayne)',
+    1                          -- OFFERED_BY (Computer Science)
+);
+
+INSERT INTO COURSE
+VALUES
+(
+    305,
+    'Software Engineering Principles',
+    3,
+    'Software Engineering (Sommerville)',
+    2                          -- OFFERED_BY (Software Engineering)
+);
+
+INSERT INTO COURSE
+VALUES
+(
+    360,
+    'Web Application Development',
+    3,
+    'Full-Stack Web Development Guide',
+    2                          -- OFFERED_BY (Software Engineering)
+);
