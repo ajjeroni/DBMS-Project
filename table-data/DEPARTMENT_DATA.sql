44e2e5ab65d8d69fbcd9f5757538116522f508f6
Adan Jeronimo <127287490+ajjeroni@users.noreply.github.com>
2025-11-26 12:15:34 -0800

    Add initial department data to DEPARTMENT_DATA.sql

---
+++ table-data/DEPARTMENT_DATA.sql
@@ -0,0 +1,19 @@
+INSERT INTO DEPARTMENT
+VALUES
+(
+    1,                     -- D_NUMBER
+    'Computer Science',    -- D_NAME
+    'CS Building 201',     -- D_LOCATION
+    '714-555-2200',        -- D_TELEPHONE
+    '012345678'            -- CHAIR_SSN (Alan Turing)
+);
+
+INSERT INTO DEPARTMENT
+VALUES
+(
+    2,
+    'Software Engineering',
+    'Engineering Hall 120',
+    '657-555-7744',
+    '987654321'            -- CHAIR_SSN (Grace Hopper)
+);
